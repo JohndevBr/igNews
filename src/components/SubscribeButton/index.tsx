b91759c32f6d15d4fc5b74754274a16e98be5b75
João Cláudio <jca.oliveira91@gmail.com>
2023-02-07 15:08:07 -0300

    Teste condicional para deploy

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -24,7 +24,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       return;
     }
 
-    if(session.activeSubscription) {
+    if(session?.activeSubscription) {
       router.push('/posts');
       return;
     } 
